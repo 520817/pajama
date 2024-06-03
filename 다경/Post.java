8718990422a699da776e16b88a75af79d33e0eec
dagyeong999 <ekrud0909@sookmyung.ac.kr>
2024-06-03 23:38:42 +0900

    Update Post.java

--- 다경/Post.java
+++ 다경/Post.java
@@ -1,108 +1,39 @@
 package com.example;
 
 import java.sql.Timestamp;
-import java.sql.Date;
 
 public class Post {
     private int id;
     private String userId;
     private String title;
-    private String category;
-    private String lCategory;
-    private Date deadline;
-    private int people;
-    private String location;
-    private String filename;
-    private String content;
-    private Timestamp createdAt;
+    private Timestamp deadline;
 
-    // getters and setters
+    // Getter and Setter
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-    
+
     public String getUserId() {
         return userId;
     }
-
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-    
-    public String getLCategory() {
-        return lCategory;
-    }
-
-    public void setLCategory(String lCategory) {
-        this.lCategory = lCategory;
-    }
-    
-    public Date getDeadline() {
+    public Timestamp getDeadline() {
         return deadline;
     }
-
-    public void setDeadline(Date deadline) {
-        this.deadline = deadline;
-    }
-    
-    public int getPeople() {
-        return people;
-    }
-
-    public void setPeople(int people) {
-        this.people = people;
-    }
-
-    public String getLocations() {
-        return location;
-    }
-
-    public void setLocations(String location) {
-        this.location = location;
-    }
-
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Timestamp getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Timestamp createdAt) {
-        this.createdAt = createdAt;
+    public void setDeadline(Timestamp deadlineTimestamp) {
+        this.deadline = deadlineTimestamp;
     }
 }
