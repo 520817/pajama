fac3fa8df391304bbe1da9237f84d95eb31b9fea
dagyeong999 <ekrud0909@sookmyung.ac.kr>
2024-06-03 23:46:26 +0900

    Update board_db00.sql

--- 다경/board_db00.sql
+++ 다경/board_db00.sql
@@ -3,9 +3,17 @@ CREATE DATABASE board_db00;
 USE board_db00;
 
 CREATE TABLE login (
-    id VARCHAR(45) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL
-);
+  id VARCHAR(45) NOT NULL,
+  pw VARCHAR(45) NOT NULL,
+  sn INT NOT NULL,
+  nn VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX id_UNIQUE (id ASC),
+  UNIQUE INDEX sn_UNIQUE (sn ASC),
+  UNIQUE INDEX nn_UNIQUE (nn ASC)
+) engine=InnoDB;
+
+insert into board_db00.login(id,pw,sn,nn) values ('love@sookmyung.ac.kr','love486',2110856,'파자마아자자2'); 
 
 CREATE TABLE posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -13,7 +21,7 @@ CREATE TABLE posts (
     title VARCHAR(255) NOT NULL,
     category VARCHAR(50) NOT NULL,
     l_category VARCHAR(50) NOT NULL,
-	deadline DATE NOT NULL,
+    deadline DATETIME NOT NULL,
     people INT NOT NULL,
     location VARCHAR(255),
     filename VARCHAR(255),
